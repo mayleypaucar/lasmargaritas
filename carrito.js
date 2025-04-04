568229b43430b3e707e08688db89d8a91df0b76f
mayleypaucar <mayleypaucarconce@gmail.com>
2025-04-03 22:24:15 -0500

    Add files via upload

--- carrito.js
+++ carrito.js
@@ -1,7 +1,7 @@
-document.querySelectorAll('.btn-primary').forEach(button => {
+document.querySelectorAll('.btn-warning').forEach(button => {
     button.addEventListener('click', function() {
         const product = this.parentElement.cloneNode(true);
-        product.querySelector('.btn-primary').remove();
+        product.querySelector('.btn-warning').remove();
         document.getElementById('cart').appendChild(product);
     });
 });
