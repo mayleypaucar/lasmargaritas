a3550db33d767f16586b2d062b91ad1a8aa9b4b0
mayleypaucar <mayleypaucarconce@gmail.com>
2025-04-10 20:49:01 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 let app = document.getElementById('typewriter');
- 
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
 });
- 
 typewriter
   .pauseFor(2500)
   .typeString('Bienvenido a Tiendas "Las Margaritas"')
